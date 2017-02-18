4f86c518b013469e629f625c87163c59cf8d595b
Alexander Owen <owen.alex47@yahoo.com>
2017-02-17 17:44:09 -0800

    Class for SymbolTable holding variable and method env info

---
+++ Project4/SymbolTable.cpp
@@ -0,0 +1,56 @@
+#include <unordered_map>
+#include <list>
+#include <string.h>
+#include "SymbolTable.h"
+
+using std::unordered_map;
+using std::list;
+
+VariableSym::VariableSym(char *i, char *t) : id(i), type(t) {}
+
+MethodSym::MethodSym(char *i, list<VariableSym *> *a, char *r) : id(i), args(a), returnType(r) {}
+
+SymbolTable::SymbolTable(SymbolTable *p) : parent(p) {}
+
+VariableSym *SymbolTable::lookupVariable(char *name)
+{
+    auto search = vMap.find(name);
+    if (search != vMap.end())
+    {
+        return search->second;
+    } 
+    else if (parent != NULL) 
+    {
+        return parent->lookupVariable(name);    
+    }
+    else
+    {
+        return NULL;
+    }
+}
+MethodSym *SymbolTable::lookupMethod(char *name)
+{
+    auto search = mMap.find(name);
+    if (search != mMap.end())
+    {
+        return search->second;
+    } 
+    else if (parent != NULL) 
+    {
+        return parent->lookupMethod(name);    
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+void SymbolTable::addVariable(char *name, VariableSym *value)
+{
+    vMap.insert({name, value});
+}
+void SymbolTable::addMethod(char *name, MethodSym *value)
+{
+    mMap.insert({name, value});
+}
+
---
+++ Project4/SymbolTable.h
@@ -0,0 +1,39 @@
+#include <unordered_map>
+#include <list>
+
+using std::unordered_map;
+using std::list;
+
+class Symbol
+{};
+
+class VariableSym : public Symbol
+{
+    public:
+        char *id;
+        char *type;
+        VariableSym(char *, char *);
+};
+
+class MethodSym : public Symbol
+{
+    public:
+        char *id;
+        list<VariableSym *> *args;
+        char *returnType;
+        MethodSym(char *, list<VariableSym *> *, char *);
+};
+
+class SymbolTable 
+{
+    public:
+        SymbolTable   *parent;
+        unordered_map<char *, VariableSym *> vMap;
+        unordered_map<char *, MethodSym *> mMap;
+
+        SymbolTable(SymbolTable *p);
+        VariableSym *lookupVariable(char *name);
+        MethodSym *lookupMethod(char *name);
+        void  addVariable(char *name, VariableSym *value); 
+        void  addMethod(char *name, MethodSym *value); 
+};
